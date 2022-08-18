1afb26f2e73c4bc3bce8ffb158b0fbc6a0cd6620
wonderarry <wonderarry@gmail.com>
2022-08-18 23:50:33 +0300

    created requests bot functionality

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import discord
 from discord import app_commands
 from discord.app_commands import Choice
 from utils.logging_setup import retrieve_logger
-from utils.config import settings
+from utils.config import settings, scope
+
+
+import gspread_asyncio
+from oauth2client.service_account import ServiceAccountCredentials
 
 main_logger = retrieve_logger(__name__)
 
@@ -28,28 +32,61 @@ class UsedClient(discord.Client):
         main_logger.info(f"Successful login as: {self.user}")
 
 
+google_client_manager = gspread_asyncio.AsyncioGspreadClientManager(lambda: ServiceAccountCredentials.from_json_keyfile_name(settings.creds_filename, scopes=scope))
 client = UsedClient()
 UsedClient.tree = app_commands.CommandTree(client)
-@UsedClient.tree.command(name='test', guild=discord.Object(id=settings.guild_id))
-async def slash(interaction: discord.Interaction, number: int, string: str):
-    await interaction.response.send_message(f'{number=} {string=}', ephemeral=True)
 
-@UsedClient.tree.command(name='asd', guild=discord.Object(id=settings.guild_id))
-@app_commands.describe(attachment='The file to upload')
-async def upload(interaction: discord.Interaction, attachment: discord.Attachment):
-    await interaction.response.send_message(f'Thanks for uploading {attachment.filename}!', ephemeral=True)
 
-@UsedClient.tree.command(name='fruit', guild=discord.Object(id=settings.guild_id))
-@app_commands.describe(fruits='fruits to choose from')
-@app_commands.choices(fruits=[
-    Choice(name='apple', value=1),
-    Choice(name='banana', value=2),
-    Choice(name='cherry', value=3),
+
+
+@UsedClient.tree.command(name='request', guild=discord.Object(id=settings.guild_id))
+@app_commands.describe(request_type='Select the request type')
+@app_commands.describe(description='Provide an in-depth description')
+@app_commands.choices(request_type=[
+    Choice(name='schedule/reschedule a match', value=0),
+    Choice(name='report a problem', value=1),
 ])
-async def fruit(interaction: discord.Interaction, fruits: Choice[int]):
-    await interaction.response.send_message(f'Your favourite fruit is {fruits.name}.')
+async def submit_request(interaction: discord.Interaction, request_type: Choice[int], description: str):
+    await interaction.response.send_message('Your message is currently being sent...', ephemeral=True)
+    session = await google_client_manager.authorize()
+    ss = await session.open_by_key(settings.sheet_id)
+    ws = await ss.get_worksheet(0)
+    
+    last_empty_row_index = len(await ws.get_all_values()) + 1
+    await ws.update_cell(last_empty_row_index, 1, request_type.value)
+    await ws.update_cell(last_empty_row_index, 2, description)
+    #await interaction.response.send_message('Your message will be delivered to the corresponding staff team member.', ephemeral=True)
+    #await interaction.response.send_message(f"You have chosen: {request_type.name}, corresponding to {request_type.value}. You are telling: {description}", ephemeral=True)
+    await interaction.edit_original_response(content='Your message was successfully delivered!')
+    
+client.run(settings.bot_token)
+
+#simple examples, to be removed
+
+# @UsedClient.tree.command(name='test', guild=discord.Object(id=settings.guild_id))
+# async def slash(interaction: discord.Interaction, number: int, string: str):
+#     await interaction.response.send_message(f'{number=} {string=}', ephemeral=True)
+
+# @UsedClient.tree.command(name='asd', guild=discord.Object(id=settings.guild_id))
+# @app_commands.describe(attachment='The file to upload')
+# async def upload(interaction: discord.Interaction, attachment: discord.Attachment):
+#     await interaction.response.send_message(f'Thanks for uploading {attachment.filename}!', ephemeral=True)
+
+
+
+
+# @UsedClient.tree.command(name='fruit', guild=discord.Object(id=settings.guild_id))
+# @app_commands.describe(fruits='fruits to choose from')
+# @app_commands.choices(fruits=[
+#     Choice(name='apple', value=1),
+#     Choice(name='banana', value=2),
+#     Choice(name='cherry', value=3),
+# ])
+# async def fruit(interaction: discord.Interaction, fruits: Choice[int]):
+#     await interaction.response.send_message(f'Your favourite fruit is {fruits.name}.')
+
+
     
         
-client.run(settings.bot_token)
 
 
--- utils/config.py
+++ utils/config.py
@@ -1,16 +1,25 @@
 from pydantic import BaseSettings
 from .logging_setup import retrieve_logger
-
+from oauth2client.service_account import ServiceAccountCredentials
 class Settings(BaseSettings):
         
     app_id: str
     guild_id: str
     pub_key: str
     bot_token: str
+    creds_filename: str
+    sheet_id: str
     
     class Config:
         env_file = '.env'
         
-
 settings = Settings()
+
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/spreadsheets",
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+
+
+
 retrieve_logger(__name__).debug("Successfully obtained settings!")
